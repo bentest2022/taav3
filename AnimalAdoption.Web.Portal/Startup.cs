32cb621f706aa88e4b705a65856dfc37a987b266
Chryogenic <b@en.vc>
2021-10-27 12:57:49 +1300

    added missing ref

--- AnimalAdoption.Web.Portal/Startup.cs
+++ AnimalAdoption.Web.Portal/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Data.SqlClient;
@@ -20,116 +21,116 @@ using System.Threading.Tasks;
 
 namespace AnimalAdoption.Web.Portal
 {
-    public class Startup
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-        private string _connectionString;
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.Configure<Configuration>(Configuration);
-
-            _connectionString = "Server=tcp:sql-azure-sprint-devsecops-1.database.windows.net,1433;Initial Catalog=sqldb-azure-sprint-devsecops-1;Persist Security Info=False;User ID=sprintadmin;Password=7ATI4lXi#Z7c;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"; //SET YOUR SQL DB CONNECTION STRING HERE
-
-            services.AddDbContext<AnimalAdoptionContext>(options =>
-            {
-                options.UseSqlServer(_connectionString);
-            });
-            
-            services.Configure<ForwardedHeadersOptions>(options =>
-            {
-                options.ForwardedHeaders = ForwardedHeaders.All;
-            });
-
-            services.AddHttpContextAccessor();
-            services.AddMemoryCache();
-            services.AddTransient<AnimalService>();
-
-            // Uncomment this section for Challenge 5
-            services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
-                .AddMicrosoftIdentityWebApp(Configuration.GetSection("AzureAd"));
-
-            services.AddRazorPages()
-                // Uncomment this section for Challenge 5
-                .AddMvcOptions(options => 
-                {
-                    var policy = new AuthorizationPolicyBuilder()
-                        .RequireAuthenticatedUser()
-                        .Build();
-                    options.Filters.Add(new AuthorizeFilter(policy));
-                })
-                .AddMicrosoftIdentityUI()
-                ;
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, AnimalAdoptionContext db)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                app.UseHttpsRedirection();
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            db.Database.EnsureCreated();
-
-            app.UseStaticFiles();
-
-            var failurePercentage = Configuration.GetValue<int?>("SimulatedFailureChance");
-            if (failurePercentage != null)
-            {
-                var rand = new Random();
-                app.Use(async (context, next) =>
-                {
-                    if (failurePercentage <= rand.Next(0, 100))
-                    {
-                        await next.Invoke();
-                    }
-                    else
-                    {
-                        throw new Exception($"A simulated failure occurred - there is a {failurePercentage}% chance of this occurring");
-                    }
-                });
-            }
-
-            if (string.IsNullOrWhiteSpace(_connectionString))
-            {
-                app.Use(async (context, next) =>
-                {
-                    var url = context.Request.Path.Value;
-                    if (!url.ToLowerInvariant().Contains("/missingenvironmentvariable"))
-                    {
-                // rewrite and continue processing
-                context.Request.Path = "/missingenvironmentvariable";
-                    }
-
-                    await next();
-                });
-            }
-
-            app.UseRouting();
-
-            // Uncomment this section for Challenge 5
-            app.UseAuthentication();
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapRazorPages();
-                endpoints.MapControllers();
-            });
-        }
+      Configuration = configuration;
     }
+
+    public IConfiguration Configuration { get; }
+    private string _connectionString;
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.Configure<Configuration>(Configuration);
+
+      _connectionString = "Server=tcp:sql-azure-sprint-devsecops-1.database.windows.net,1433;Initial Catalog=sqldb-azure-sprint-devsecops-1;Persist Security Info=False;User ID=sprintadmin;Password=7ATI4lXi#Z7c;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"; //SET YOUR SQL DB CONNECTION STRING HERE
+
+      services.AddDbContext<AnimalAdoptionContext>(options =>
+      {
+        options.UseSqlServer(_connectionString);
+      });
+
+      services.Configure<ForwardedHeadersOptions>(options =>
+      {
+        options.ForwardedHeaders = ForwardedHeaders.All;
+      });
+
+      services.AddHttpContextAccessor();
+      services.AddMemoryCache();
+      services.AddTransient<AnimalService>();
+
+      // Uncomment this section for Challenge 5
+      services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+          .AddMicrosoftIdentityWebApp(Configuration.GetSection("AzureAd"));
+
+      services.AddRazorPages()
+          // Uncomment this section for Challenge 5
+          .AddMvcOptions(options =>
+          {
+            var policy = new AuthorizationPolicyBuilder()
+                      .RequireAuthenticatedUser()
+                      .Build();
+            options.Filters.Add(new AuthorizeFilter(policy));
+          })
+          .AddMicrosoftIdentityUI()
+          ;
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env, AnimalAdoptionContext db)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+      else
+      {
+        app.UseExceptionHandler("/Error");
+        app.UseHttpsRedirection();
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+      }
+
+      db.Database.EnsureCreated();
+
+      app.UseStaticFiles();
+
+      var failurePercentage = Configuration.GetValue<int?>("SimulatedFailureChance");
+      if (failurePercentage != null)
+      {
+        var rand = new Random();
+        app.Use(async (context, next) =>
+        {
+          if (failurePercentage <= rand.Next(0, 100))
+          {
+            await next.Invoke();
+          }
+          else
+          {
+            throw new Exception($"A simulated failure occurred - there is a {failurePercentage}% chance of this occurring");
+          }
+        });
+      }
+
+      if (string.IsNullOrWhiteSpace(_connectionString))
+      {
+        app.Use(async (context, next) =>
+        {
+          var url = context.Request.Path.Value;
+          if (!url.ToLowerInvariant().Contains("/missingenvironmentvariable"))
+          {
+                    // rewrite and continue processing
+                    context.Request.Path = "/missingenvironmentvariable";
+          }
+
+          await next();
+        });
+      }
+
+      app.UseRouting();
+
+      // Uncomment this section for Challenge 5
+      app.UseAuthentication();
+      app.UseAuthorization();
+
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapRazorPages();
+        endpoints.MapControllers();
+      });
+    }
+  }
 }
